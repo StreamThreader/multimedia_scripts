eccbc6a85af716367a70382ed959243997e10d2b
Stream <kvantarium@gmail.com>
2025-02-25 13:33:36 +0200

    Update yt-download-mp3.ps1

--- yt-download-mp3.ps1
+++ yt-download-mp3.ps1
@@ -1,3 +1,11 @@
 $yt_url=$args[0]
 
-yt-dlp -f 'ba' -x --audio-format mp3 --audio-quality 0 --embed-thumbnail --convert-thumbnails jpg --embed-metadata --restrict-filenames --windows-filenames -o "%(uploader)s_-_%(title)s.%(ext)s" $yt_url
+yt-dlp -f 'ba' -x `
+    --audio-format mp3 `
+    --audio-quality 0 `
+    --embed-thumbnail `
+    --convert-thumbnails jpg `
+    --embed-metadata `
+    --restrict-filenames `
+    --windows-filenames `
+    -o "%(uploader)s_-_%(title)s.%(ext)s" $yt_url
